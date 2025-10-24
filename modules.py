8f179d6a6e4b17fee969b8581e7a53f66bb85f8e
neil-nb <nbarigye@live.com>
2025-10-24 16:12:11 +1000

    Added spatial squeeze excitation block.

--- modules.py
+++ modules.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch
 
 class ConvBNAct(nn.Module):
     def __init__(self, in_ch, out_ch, k=3, s=1, p="same", groups=1, act=True):
@@ -18,7 +19,7 @@ class ResidualBlock(nn.Module):
         self.conv2 = ConvBNAct(out_ch, out_ch, act=False)
         self.drop = nn.Dropout2d(p=dropout_p) if dropout_p > 0 else nn.Identity()
         self.act = nn.ReLU(inplace=True)
-        
+
     def forward(self, x):
         residual = self.proj(x)
         x = self.conv1(x)
@@ -26,3 +27,11 @@ class ResidualBlock(nn.Module):
         x = self.conv2(x)
         x = x + residual
         return self.act(x)
+
+class sSE(nn.Module):
+    def __init__(self, ch):
+        super().__init__()
+        self.conv = nn.Conv2d(ch, 1, kernel_size=1)
+
+    def forward(self, x):
+        return x * torch.sigmoid(self.conv(x))
