45163e25ee551416776a6b6b65975c1f84d4aeb0
neil-nb <nbarigye@live.com>
2025-10-24 15:22:59 +1000

    Added tensor preparation helper method with resizing for images and masks.

--- dataset.py
+++ dataset.py
@@ -1,7 +1,9 @@
 import os
+import torch
 from torch.utils.data import Dataset
 import nibabel as nib
 import numpy as np
+import torch.nn.functional as F
 
 class HipMRIDataset(Dataset):
     def __init__(self, img_dir, mask_dir, apply_transform=None, standardise=False, output_shape=(256, 128)):
@@ -20,3 +22,11 @@ class HipMRIDataset(Dataset):
 
     def _read_nifti(self, path):
         return nib.load(path).get_fdata(dtype=np.float32)
+    
+    def _prepare_tensor(self, array, is_mask=False):
+        tensor = torch.as_tensor(array, dtype=torch.float32).unsqueeze(0)
+        if is_mask:
+            tensor = F.interpolate(tensor.unsqueeze(0), size=self.output_shape, mode='nearest').squeeze(0).long()
+        else:
+            tensor = F.interpolate(tensor.unsqueeze(0), size=self.output_shape, mode='bilinear', align_corners=False).squeeze(0)
+        return tensor
